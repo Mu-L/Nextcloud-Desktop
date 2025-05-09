e4641726304b09090345d879f3551b2af4e2b483
Claudio Cambra <claudio.cambra@nextcloud.com>
2025-05-09 11:24:02 +0800

    feat(gui/macOS): Add method to send ignore list over XPC interface
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- src/gui/macOS/fileproviderxpc.h
+++ src/gui/macOS/fileproviderxpc.h
@@ -38,6 +38,7 @@ public slots:
     void unauthenticateExtension(const QString &extensionAccountId) const;
     void createDebugArchiveForExtension(const QString &extensionAccountId, const QString &filename);
 
+    void setIgnoreList() const;
     void setFastEnumerationEnabledForExtension(const QString &extensionAccountId, bool enabled) const;
 
 private slots:
